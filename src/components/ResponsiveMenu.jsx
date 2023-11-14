f862c6b0e45a86694c5eb409e78e02dde01bb2dc
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2023-11-14 13:08:59 -0500

    nav

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { CiMenuBurger } from "react-icons/ci";
+import { TfiClose } from "react-icons/tfi";
 
 export default function ResponsiveMenu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,19 +12,19 @@ export default function ResponsiveMenu() {
 
   return (
     <>
-      <button className="md:hidden" onClick={toggleMenu}>
-        <svg viewBox="0 0 100 80" width="40" height="40">
-          <rect width="100" height="20"></rect>
-          <rect y="30" width="100" height="20"></rect>
-          <rect y="60" width="100" height="20"></rect>
-        </svg>
+      <button className="md:hidden absolute top-5 right-8" onClick={toggleMenu}>
+        {isMenuOpen ? <TfiClose size={30} /> : <CiMenuBurger size={30} />}
       </button>
-      <div className={`col-span-9 md:flex ${isMenuOpen ? 'flex' : 'hidden'} justify-between space-x-6`}>
-        <a href="/about" className="menu-link">Sobre</a>
-        <a href="/archi" className="menu-link">Arquitetura</a>
-        <a href="/design" className="menu-link">Design</a>
-        <a href="/store" className="menu-link">Loja</a>
-        <a href="/contact" className="menu-link">Contato</a>
+      <div className={`col-span-9 md:flex ${isMenuOpen ? 'flex' : 'hidden'} flex-col md:flex-row md:justify-between space-y-4 md:space-y-0 md:space-x-6`}>
+        <div class="flex flex-col md:flex-row md:col-span-6 justify-start space-y-4 md:space-y-0 md:space-x-8">
+          <a href="/about" class="menu-link">Sobre</a>
+          <a href="/archi" class="menu-link">Arquitetura</a>
+          <a href="/design" class="menu-link">Design</a>
+        </div>
+        <div class="flex flex-col md:flex-row md:col-span-3 justify-end space-y-4 md:space-y-0 md:space-x-8">
+          <a href="/store" class="menu-link">Loja</a>
+          <a href="/contact" class="menu-link">Contato</a>
+        </div>
       </div>
     </>
   );
